befe8a06da68d7e5587135767615e95840eb7ea4
Vyacheslavchik <clavik_2000@mail.ru>
2024-01-31 23:14:27 +0300

    Задача №1

--- Program.cs
+++ Program.cs
@@ -18,47 +18,47 @@ namespace techpoint.ozon
 
         }
 
-        //static void numberOne()
-        //{
-        //    int a = int.Parse(Console.ReadLine());
-        //    string[] array = new string[a];
-        //    for(int i = 0; i < a; i++)
-        //    {
-        //        array[i] = ShipInspection(Console.ReadLine());
-        //    }
-        //    for (int i = 0; i < a; i++)
-        //    {
-        //       Console.WriteLine(array[i]);
-        //    }
+        static void numberOne()
+        {
+            int a = int.Parse(Console.ReadLine());
+            string[] array = new string[a];
+            for (int i = 0; i < a; i++)
+            {
+                array[i] = ShipInspection(Console.ReadLine());
+            }
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
 
-        //}
-        //static string ShipInspection(string word)
-        //{
-        //    int one = 0;
-        //    int two = 0;
-        //    int tree = 0;
-        //    int four = 0;
-        //    for(int i = 0; i < 19; i+=2)
-        //    {
-        //        switch (word[i])
-        //        {
-        //            case '1':
-        //                four++;
-        //                break;
-        //            case '2':
-        //                tree++;
-        //                break;
-        //            case '3':
-        //                two++;
-        //                break;
-        //            case '4':
-        //                one++;
-        //                break;
-        //        }
-        //        if (one > 1 || two > 2 || tree > 3 || four > 4)
-        //            return "NO";
-        //    }
-        //    return "YES";
-        //}
+        }
+        static string ShipInspection(string word)
+        {
+            int one = 0;
+            int two = 0;
+            int tree = 0;
+            int four = 0;
+            for (int i = 0; i < 19; i += 2)
+            {
+                switch (word[i])
+                {
+                    case '1':
+                        four++;
+                        break;
+                    case '2':
+                        tree++;
+                        break;
+                    case '3':
+                        two++;
+                        break;
+                    case '4':
+                        one++;
+                        break;
+                }
+                if (one > 1 || two > 2 || tree > 3 || four > 4)
+                    return "NO";
+            }
+            return "YES";
+        }
     }
 }
