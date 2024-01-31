83a20128cd9bae66cf8922f70e4b4f867fbd4d2e
Vyacheslavchik <clavik_2000@mail.ru>
2024-01-31 23:11:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace techpoint.ozon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            numberTwo();
+        }
+
+        static void numberTwo()
+        {
+
+        }
+
+        //static void numberOne()
+        //{
+        //    int a = int.Parse(Console.ReadLine());
+        //    string[] array = new string[a];
+        //    for(int i = 0; i < a; i++)
+        //    {
+        //        array[i] = ShipInspection(Console.ReadLine());
+        //    }
+        //    for (int i = 0; i < a; i++)
+        //    {
+        //       Console.WriteLine(array[i]);
+        //    }
+
+        //}
+        //static string ShipInspection(string word)
+        //{
+        //    int one = 0;
+        //    int two = 0;
+        //    int tree = 0;
+        //    int four = 0;
+        //    for(int i = 0; i < 19; i+=2)
+        //    {
+        //        switch (word[i])
+        //        {
+        //            case '1':
+        //                four++;
+        //                break;
+        //            case '2':
+        //                tree++;
+        //                break;
+        //            case '3':
+        //                two++;
+        //                break;
+        //            case '4':
+        //                one++;
+        //                break;
+        //        }
+        //        if (one > 1 || two > 2 || tree > 3 || four > 4)
+        //            return "NO";
+        //    }
+        //    return "YES";
+        //}
+    }
+}
