614607ae73b1170af891318b4d2611c81f66604a
Pavlov-VA <vap.uspeg@gmail.com>
2024-02-09 15:38:16 +0300

    начало решения

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,153 +11,47 @@ namespace techpoint.ozon
     {
         static void Main(string[] args)
         {
-            
-        }
-
-
-        //Номер 1: Морской бой
-        static void numberOne()
-        {
             int a = int.Parse(Console.ReadLine());
             string[] array = new string[a];
             for (int i = 0; i < a; i++)
             {
-                array[i] = ShipInspection(Console.ReadLine());
+                array[i] = Console.ReadLine();
             }
-            for (int i = 0; i < a; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
-
         }
-        static string ShipInspection(string word)
+    }
+    class Figur
+    {
+        int ax;
+        int ay;
+        int bx;
+        int by;
+        int cx;
+        int cy;
+        int dx;
+        int dy;
+        Figur dad;
+        Figur son;
+        int valueDady;
+        public Figur(int ax, int ay, string[]pole)
         {
-            int one = 0;
-            int two = 0;
-            int tree = 0;
-            int four = 0;
-            for (int i = 0; i < 19; i += 2)
+            this.ax = ax;
+            this.ay = ay;
+            int i=ax;
+            while (pole[ay][i] == '*' && ax < pole[ax].Length)
             {
-                switch (word[i])
-                {
-                    case '1':
-                        four++;
-                        break;
-                    case '2':
-                        tree++;
-                        break;
-                    case '3':
-                        two++;
-                        break;
-                    case '4':
-                        one++;
-                        break;
-                }
-                if (one > 1 || two > 2 || tree > 3 || four > 4)
-                    return "NO";
+                i++;
             }
-            return "YES";
+            bx = i-1;
+            by = ay;
+            i = ay;
+            while (pole[i][ax] == '*' && ax < pole.Length)
+            {
+                i++;
+            }
+            cy = i - 1;
+            cx = i - 1;
+            dx = bx;
+            dy = cy;
         }
-
-        //Номер 2: Проверка даты
-        static void numberTwo()
-        {
-            int a = int.Parse(Console.ReadLine());
-            int[,] array = new int[a, 3];
-            for (int i = 0; i < a; i++)
-            {
-                string[] words = Console.ReadLine().Split(' ');
-                array[i, 0] = int.Parse(words[0]);
-                array[i, 1] = int.Parse(words[1]);
-                array[i, 2] = int.Parse(words[2]);
-            }
-            for (int i = 0; i < a; i++)
-            {
-                try
-                {
-                    DateTime dateTime = new DateTime(array[i, 2], array[i, 1], array[i, 0]);
-                    Console.WriteLine("Yes");
-                }
-                catch (ArgumentOutOfRangeException e)
-                {
-                    Console.WriteLine("No");
-                }
-            }
-
-        }
-
-        //Номер 3: Автомобильные номера
-        static void numberTree()
-        {
-            int a = int.Parse(Console.ReadLine());
-            string[] array = new string[a];
-            for (int i = 0; i < a; i++)
-            {
-                array[i] = findNumber(Console.ReadLine());
-            }
-
-            for (int i = 0; i < a; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
-        }
-        static string findNumber(string line)
-        {
-            StringBuilder newLine = new StringBuilder();
-            for (int i = 0; i < line.Length; i++)
-            {
-                if (i + 4 <= line.Length && char.IsLetter(line[i]) && char.IsDigit(line[i + 1]))
-                {
-
-                    if (i + 5 <= line.Length && char.IsDigit(line[i + 2]) && char.IsLetter(line[i + 3]) && char.IsLetter(line[i + 4]))
-                    {
-                        newLine.Append(line[i].ToString() + line[i + 1].ToString() + line[i + 2].ToString() + line[i + 3].ToString() + line[i + 4].ToString() + " ");
-                        i = i + 4;
-                    }
-                    else if (char.IsLetter(line[i + 2]) && char.IsLetter(line[i + 3]))
-                    {
-                        newLine.Append(line[i].ToString() + line[i + 1].ToString() + line[i + 2].ToString() + line[i + 3].ToString() + " ");
-                        i = i + 3;
-                    }
-                    else
-                        return "-";
-                }
-                else
-                    return "-";
-            }
-
-            return newLine.ToString();
-        }
-
-
-
-        //Номер 4: Битва за кондиционер
-        static void numberFour()
-        {
-            int countTests = int.Parse(Console.ReadLine());
-            int[][] ansvres = new int[countTests][];
-            for (int q = 0; q < countTests; q++)
-            {
-                int valueArrays = int.Parse(Console.ReadLine());
-                int[] values = new int[valueArrays];
-                int leftValue = 15;
-                int rightValue = 30;
-                for (int i = 0; i < valueArrays; i++)
-                {
-                    string[] words = Console.ReadLine().Split();
-                    if (words[0].Equals("<=")) rightValue = int.Parse(words[1]) < rightValue ? int.Parse(words[1]) : rightValue;
-                    if (words[0].Equals(">=")) leftValue = int.Parse(words[1]) > leftValue ? int.Parse(words[1]) : leftValue;
-                    values[i] = leftValue <= rightValue ? leftValue : -1;
-                }
-                ansvres[q] = values;
-            }
-            for (int i = 0; i < ansvres.Length; i++)
-            {
-                for (int q = 0; q < ansvres[i].Length; q++)
-                {
-                    Console.WriteLine(ansvres[i][q]);
-                }
-                Console.WriteLine();
-            }
-        }
+    }
 }
